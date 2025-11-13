8c702dc216c29b247c4e9ec8efc01f044be96139
samuel <legosthes@gmail.com>
2025-11-13 17:19:53 +0800

    feat: add read and create todos apis

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ app = FastAPI()
 
 class ToDo(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    item: str = Field(index=True, max_length=200)
+    item: str = Field(max_length=200)
     is_completed: bool = Field(default=False)
-    created_at: datetime = Field(default_factory=datetime.now, nullable=False)
-    updated_at: datetime = Field(default_factory=datetime.now, nullable=False)
+    created_at: str = Field(default_factory=datetime.now, nullable=False)
+    updated_at: str = Field(default_factory=datetime.now, nullable=False)
 
 
 sqlite_file_name = "todolist.db"
@@ -36,3 +36,21 @@ SessionDep = Annotated[Session, Depends(get_session)]
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+
+
+@app.get("/todos/")
+def read_todos(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[ToDo]:
+    todos = session.exec(select(ToDo).offset(offset).limit(limit)).all()
+    return todos
+
+
+@app.post("/todos/")
+def create_todo(todo: ToDo, session: SessionDep) -> ToDo:
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+    return todo
