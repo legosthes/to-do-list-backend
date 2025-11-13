548da5e1b235a0009f53e87d220401adaaef799d
samuel <legosthes@gmail.com>
2025-11-13 16:06:44 +0800

    feat: create todo database model

--- main.py
+++ main.py
@@ -1,42 +1,38 @@
-from typing import Union
-
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from datetime import datetime
+from typing import Annotated
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+class ToDo(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    item: str = Field(index=True, max_length=200)
+    is_completed: bool = Field(default=False)
+    created_at: datetime = Field(default_factory=datetime.now, nullable=False)
+    updated_at: datetime = Field(default_factory=datetime.now, nullable=False)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+sqlite_file_name = "todolist.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args, echo=True)
 
 
-@app.get("/items/")
-def read_all_items(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 
-@app.post("/items/new/")
-def new_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+SessionDep = Annotated[Session, Depends(get_session)]
 
 
-@app.put("/items/{item_id}/edit")
-def edit_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.delete("/items/{item_id}/delete")
-def delete_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
